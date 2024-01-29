69b7b61c68046d4c4078a29a93aa45d148be4fa4
Jenna Ritvanen <jenna.ritvanen@fmi.fi>
2024-01-29 13:31:39 +0200

    1st version of weighted avg of det ans ens forecasts

--- interpolated_forecast_to_acrr.py
+++ interpolated_forecast_to_acrr.py
@@ -92,9 +92,22 @@ def run(timestamp, config):
     timestep = conf["input"]["ensemble"]["data"]["timeres"]
 
     accumulation_times = utils.determine_accumulation_times(
-        curdate, conf, fc_len=conf["output"]["accumulations"]["fc_len"]
+        curdate, conf, fc_len=conf["output"]["accumulations"]["timeconfig"]["fc_len"]
     )
-    accumulation_timestep = conf["output"]["accumulations"]["timestep"]
+    accumulation_timestep = conf["output"]["accumulations"]["timeconfig"]["timestep"]
+
+    ensemble_members = list(range(1, conf["input"]["ensemble"]["data"]["n_ens_members"] + 1))
+    ensemble_members_det = ensemble_members + ["det"]
+
+    # Weighting of deterministic and ensemble
+    determ_initweight = conf["output"]["accumulations"]["weighted_mean_det_ens"]["determ_initweight"]
+    determ_weightspan = conf["output"]["accumulations"]["weighted_mean_det_ens"]["determ_weightspan"]
+    determ_startw = 0.01 * determ_initweight * conf["input"]["ensemble"]["data"]["n_ens_members"]
+    n_timesteps = int(
+        conf["output"]["accumulations"]["timeconfig"]["fc_len"]
+        / conf["output"]["accumulations"]["timeconfig"]["timeres"]
+    )
+    determ_lapse = 100.0 * determ_startw / (determ_weightspan * n_timesteps)
 
     FILE_DICT_ACCUM = None
     # Calculate accumulations for the times that are needed
@@ -108,55 +121,20 @@ def run(timestamp, config):
             inclusive="right",
         ).to_pydatetime()
 
+        # Weights of deterministic forecast
+        leadtime_indices = np.array([t.total_seconds() / 60 / timestep for t in acrr_timesteps - curdate])
+
         accrs = {}
         nodata_masks = {}
         undetect_masks = {}
         logging.info(f"Processing accumulation timestep {start}")
 
-        # Load deterministic prediction
-        det_arrs = {}
-        for tt in acrr_timesteps:
-            # If timestep before start of deterministic prediction, read
-            # the interpolated observation from first ensemble member
-            if tt < curdate:
-                ensno_ = 1
-                arr, _, _, file_dict_accum = load_ensemble_file(
-                    ensno_,
-                    tt,
-                    curdate,
-                    conf,
-                    config,
-                    file_dict_accum=None,
-                )
-                if arr is not None:
-                    det_arrs[tt] = arr
-                if FILE_DICT_ACCUM is None:
-                    FILE_DICT_ACCUM = file_dict_accum
-                continue
-
-            # import ipdb
-
-            # ipdb.set_trace()
-            arr, _, _, file_dict_accum = load_det_file(
-                tt,
-                curdate,
-                conf,
-                config,
-                file_dict_accum=None,
-            )
-            if arr is not None:
-                det_arrs[tt] = arr
-            if FILE_DICT_ACCUM is None:
-                FILE_DICT_ACCUM = file_dict_accum
-
         missing_ensemble_members = []
-        for ensno_ in range(1, conf["input"]["ensemble"]["data"]["n_ens_members"] + 1):
-            interp_arrs = {}
+        interp_arrs = {}
+        for ensno_ in ensemble_members_det:
+            interp_arrs[ensno_] = {}
             for tt in acrr_timesteps:
                 # Read ensemble member
-                # import pdb
-
-                # pdb.set_trace()
                 arr, _, _, file_dict_accum = load_ensemble_file(
                     ensno_,
                     tt,
@@ -166,7 +144,7 @@ def run(timestamp, config):
                     file_dict_accum=None,
                 )
                 if arr is not None:
-                    interp_arrs[tt] = arr
+                    interp_arrs[ensno_][tt] = arr
                 if FILE_DICT_ACCUM is None:
                     FILE_DICT_ACCUM = file_dict_accum
 
@@ -174,7 +152,7 @@ def run(timestamp, config):
             logging.debug(f"Processing ensemble member {ensno_}/{conf['input']['ensemble']['data']['n_ens_members']}")
             # Get keys that are in the interval
             # TODO if this is too low, we should reject the accumulation as invalid
-            keys_in_interval = [k for k in interp_arrs.keys() if k > start and k <= end]
+            keys_in_interval = [k for k in interp_arrs[ensno_].keys() if k > start and k <= end]
 
             logging.debug(f"No of keys in interval: {len(keys_in_interval)}")
             if len(keys_in_interval) < accumulation_timestep / timestep:
@@ -190,38 +168,36 @@ def run(timestamp, config):
                 continue
 
             # Get accumulation
-            arrs = [interp_arrs[k] for k in keys_in_interval]
+            arrs = [interp_arrs[ensno_][k] for k in keys_in_interval]
             accrs[ensno_] = np.nansum(arrs, axis=0)
 
             # Get nodata and undetect masks
-            nodata_arrs = [np.isnan(interp_arrs[k]) for k in keys_in_interval]
-            undetect_arrs = [interp_arrs[k] == 0 for k in keys_in_interval]
+            nodata_arrs = [np.isnan(interp_arrs[ensno_][k]) for k in keys_in_interval]
+            undetect_arrs = [interp_arrs[ensno_][k] == 0 for k in keys_in_interval]
             # TODO figure out if here should be any() or all()
             nodata_masks[ensno_] = np.all(nodata_arrs, axis=0)
             undetect_masks[ensno_] = np.all(undetect_arrs, axis=0)
 
         # Ensemble mean for the accumulation
-        ens_mean = np.nanmean([accrs[k] for k in accrs.keys()], axis=0)
-        ens_nodata_mask = np.all([nodata_masks[k] for k in accrs.keys()], axis=0)
-        ens_undetect_mask = np.all([undetect_masks[k] for k in accrs.keys()], axis=0)
+        ens_mean = np.nanmean([accrs[k] for k in ensemble_members], axis=0)
+        ens_nodata_mask = np.all([nodata_masks[k] for k in ensemble_members], axis=0)
+        ens_undetect_mask = np.all([undetect_masks[k] for k in ensemble_members], axis=0)
         ens_mean_ = utils.convert_dtype(
             ens_mean,
-            conf["output"]["accumulations"],
+            conf["output"]["accumulations"]["ensemble_mean"],
             ens_nodata_mask,
             ens_undetect_mask,
         )
 
-        # Write accumulation to file
-        outfile = Path(
-            conf["output"]["accumulations"]["dir"]
-            + "/"
-            + conf["output"]["accumulations"]["filename"].format(
-                timestamp=f"{timestamp}00",
-                fc_timestep=f"{(end - curdate).total_seconds() / 60:.0f}",
-                fc_timestamp=f"{end:%Y%m%d%H%M}00",
-                acc_timestep=f'{conf["output"]["accumulations"]["timestep"]:03}',
-                config=config,
-            )
+        # Write ensemble accumulation to file
+        outfile = Path(conf["output"]["accumulations"]["ensemble_mean"]["dir"]) / conf["output"]["accumulations"][
+            "ensemble_mean"
+        ]["filename"].format(
+            timestamp=f"{timestamp}",
+            fc_timestep=f"{(end - curdate).total_seconds() / 60:.0f}",
+            fc_timestamp=f"{end:%Y%m%d%H%M}",
+            acc_timestep=f'{conf["output"]["accumulations"]["timeconfig"]["timestep"]:03}',
+            config=config,
         )
         enddate = f"{end:%Y%m%d}"
         endtime = f"{end:%H%M}"
@@ -238,11 +214,74 @@ def run(timestamp, config):
             starttime,
             enddate,
             endtime,
-            conf["output"]["accumulations"],
+            conf["output"]["accumulations"]["ensemble_mean"],
+        )
+
+        # weighted average of deterministic and ensemble
+        accr_arrays_det = np.stack([interp_arrs["det"][k] for i, k in enumerate(acrr_timesteps)])
+        accr_weights_det = determ_startw - determ_lapse * leadtime_indices
+
+        accr_arrays_ens = np.stack(
+            [[interp_arrs[ensno_][k] for i, k in enumerate(acrr_timesteps)] for ensno_ in ensemble_members]
+        )
+        accr_arrays = np.concatenate([accr_arrays_det[np.newaxis, :, :], accr_arrays_ens], axis=0)
+
+        weights = np.concatenate(
+            [accr_weights_det[np.newaxis, :], np.ones((len(ensemble_members), len(acrr_timesteps)))], axis=0
+        )
+
+        # for loop version
+        # ens_det_accr = np.zeros_like(ens_mean)
+        # for t in range(len(acrr_timesteps)):
+        #     avg_accr_t = np.average(accr_arrays[:, t, :, :], weights=weights[:, t], axis=0)
+        #     ens_det_accr += avg_accr_t
+        w1 = weights[:, :, None, None]
+        scl = np.sum(w1, axis=0)
+        avg1 = np.multiply(accr_arrays, w1).sum(axis=0) / scl
+
+        # test equality with loop version
+        # mask = ~(np.isnan(accr_weighted) | np.isnan(ens_det_accr))
+        # np.allclose(accr_weighted[mask], ens_det_accr[mask])
+
+        accr_weighted = np.sum(avg1, axis=0)
+        # TODO update nodata masks
+        accr_weighted_ = utils.convert_dtype(
+            accr_weighted,
+            conf["output"]["accumulations"]["weighted_mean_det_ens"],
+            ens_nodata_mask,
+            ens_undetect_mask,
+        )
+        # Write to file
+        outfile = Path(conf["output"]["accumulations"]["weighted_mean_det_ens"]["dir"]) / conf["output"][
+            "accumulations"
+        ]["weighted_mean_det_ens"]["filename"].format(
+            timestamp=f"{timestamp}00",
+            fc_timestep=f"{(end - curdate).total_seconds() / 60:.0f}",
+            fc_timestamp=f"{end:%Y%m%d%H%M}00",
+            acc_timestep=f'{conf["output"]["accumulations"]["timeconfig"]["timestep"]:03}',
+            config=config,
+        )
+        utils.write_accumulated_h5(
+            outfile,
+            accr_weighted_,
+            FILE_DICT_ACCUM,
+            enddate,
+            endtime,
+            startdate,
+            starttime,
+            enddate,
+            endtime,
+            conf["output"]["accumulations"]["weighted_mean_det_ens"],
         )
 
         # Write nodata and undetect masks to file
-        nodata_outfile = outfile.parent / (outfile.stem + "_nodata_mask.h5")
+        nodata_outfile = Path(conf["output"]["nodata"]["dir"]) / conf["output"]["nodata"]["filename"].format(
+            timestamp=f"{timestamp}00",
+            fc_timestep=f"{(end - curdate).total_seconds() / 60:.0f}",
+            fc_timestamp=f"{end:%Y%m%d%H%M}00",
+            acc_timestep=f'{conf["output"]["accumulations"]["timeconfig"]["timestep"]:03}',
+            config=config,
+        )
         ens_nodata_mask_ = utils.convert_dtype(
             ens_nodata_mask,
             conf["output"]["nodata"],
@@ -264,7 +303,13 @@ def run(timestamp, config):
             quantity="nodata_mask",
         )
 
-        undetect_outfile = outfile.parent / (outfile.stem + "_undetect_mask.h5")
+        undetect_outfile = Path(conf["output"]["undetect"]["dir"]) / conf["output"]["undetect"]["filename"].format(
+            timestamp=f"{timestamp}00",
+            fc_timestep=f"{(end - curdate).total_seconds() / 60:.0f}",
+            fc_timestamp=f"{end:%Y%m%d%H%M}00",
+            acc_timestep=f'{conf["output"]["accumulations"]["timeconfig"]["timestep"]:03}',
+            config=config,
+        )
         ens_undetect_mask_ = utils.convert_dtype(
             ens_undetect_mask,
             conf["output"]["undetect"],
