101b43bb654072e537cd85c842b5c60e48d93717
Jenna Ritvanen <jenna.ritvanen@fmi.fi>
2024-10-14 09:53:29 +0300

    Add default 5mins for allowed timediff

--- utils.py
+++ utils.py
@@ -56,19 +56,35 @@ def read_conf(config_file):
 def read_snowprob(curdate, snowprob_conf):
     """Read probability of snow data.
 
-    Allows searching some timesteps bawckwards, if file does not exist for the current timestep.
+    Allows searching some timesteps bawckwards, if file does not exist for
+    the current timestep. The allowed time difference should be defined
+    as minutes with the key "allow_timediff" in the snowprob_conf;
+    otherwise 5 minutes is used.
+
+    Parameters
+    ----------
+    curdate : datetime
+        The current time.
+    snowprob_conf : dict
+        The configuration for the snow probability data. Should include keys
+        "dir", "filename", "timeres", "allow_timediff".
+
+    Returns
+    -------
+    np.ndarray
+        The snow probability data.
 
     """
-
     path = Path(snowprob_conf["dir"])
 
     curfile = path / snowprob_conf["filename"].format(timestamp=curdate.strftime("%Y%m%d%H%M"))
+    allowed_timediff = snowprob_conf.get("allow_timediff", 5) * 60
 
     prev_time = curdate
     while not curfile.exists():
         # Find the previous file
         timediff = curdate - prev_time
-        if timediff.total_seconds() > (snowprob_conf["allow_timediff"] * 60):
+        if timediff.total_seconds() > allowed_timediff:
             raise FileNotFoundError(
                 f"Could not find snow probability file for {curdate} or older, tried up to {prev_time}"
             )
